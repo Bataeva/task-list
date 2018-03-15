e63d7e416eca4ab44c2a9f9d7b561f70720e33d6
Bataeva <bataeva09@icloud.com>
2018-03-15 10:36:32 +0300

    Create category.index page

--- app/Http/Controllers/Admin/TaskController.php
+++ app/Http/Controllers/Admin/TaskController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\Admin;
 use App\Task;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Controller;
 
 class TaskController extends Controller
 {
